9e167f110b8d40d9595993c7b146685ed6c4b28f
sandovalbento <sandovalbento@gmail.com>
2026-06-20 22:06:30 -0300

    ajuste

--- src/main/java/br/com/fiap/food/application/gateways/ItemCardapioGatewaySpec.java
+++ src/main/java/br/com/fiap/food/application/gateways/ItemCardapioGatewaySpec.java
@@ -8,13 +8,9 @@ import java.util.List;
 public interface ItemCardapioGatewaySpec {
 
    ItemCardapioResponse criar(ItemCardapioRequest request);
-
    ItemCardapioResponse buscarPorId(Long id);
-
     List<ItemCardapioResponse> listarTodos();
-
     ItemCardapioResponse atualizar(Long id, ItemCardapioRequest request);
-
     void deletar(Long id);
 }
 
